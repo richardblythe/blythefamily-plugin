1140ddad0732e852d270692a389474c57d0be022
richardblythe <richardblythe84@gmail.com>
2020-10-05 22:14:45 -0500

    updates to the style

--- blythefamily.php
+++ blythefamily.php
@@ -3,7 +3,7 @@
     Plugin Name: Blythe Family
     Plugin URI: http://www.blythefamily.com/
     Description: Structural components for the Blythe Family website
-    Version: 1.10
+    Version: 1.11
     Author: Richard Blythe
     Author URI: http://unity3software.com/richardblythe
     GitHub Plugin URI: https://github.com/richardblythe/blythefamily-plugin
@@ -16,7 +16,7 @@ class BF {
     	$debug = (defined('WP_DEBUG') && true === WP_DEBUG);
 
         //TODO replace time
-	    BF::$ver = time(); //'1.0';
+	    BF::$ver = '1.11';
         BF::$dir = plugin_dir_path( __FILE__ );
         BF::$url = plugin_dir_url( __FILE__ );
 	    BF::$assets_url = BF::$url . 'assets';
